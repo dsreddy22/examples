fdd67631232e73b65abed84ed046b111752920a3
Kondamuri <sankararao.kondamuri@getronics.com>
2018-09-05 16:13:18 +0530

    final commit

--- web_apps.tf
+++ web_apps.tf
@@ -1,54 +1,16 @@
-resource "random_id" "server" {
-  keepers = {
-    azi_id = 1
-  }
-
-  byte_length = 6
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
-}
-
-resource "azurerm_app_service_plan" "serviceplan" {
-  name                = "app-service-plan"
-  location 	      = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
 resource "azurerm_app_service" "webapp" {
+  source = "git::https://github.com/dsreddy22/modules.git"
   count 	      = "${length(var.webapps)}"
-  name                = "${var.webapps[count.index]}-${random_id.server.hex}"
+  name                = "${var.webapps[count.index]}-${module.web_app.name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  app_service_plan_id = "${azurerm_app_service_plan.serviceplan.id}"
+  app_service_plan_id = "${module.web_app.app_service_plan_id}"
 
   site_config {
-    dotnet_framework_version = "${var.dotnet_framework_version}"
-    scm_type                 = "${var.scm_type}"
-
-  source_control {
-    repo_url = "${var.repourl}"
-    branch   = "${var.branch}"
+    dotnet_framework_version = "${module.web_app.site_config.dotnet_framework_version}"
 }  
 }
 /*
-source_control {
-
-repo_url = "${var.repourl}"
-branch   = "${var.branch}"
-}
-
-site_credential {
-username = "${var.siteuser}"
-password = "${var.sitesecret}"
-}
 
 connection_string {
 name  = "${var.connect_string_name}"
@@ -57,4 +19,3 @@ value = "${var.connect_string_value}"
 username = "${var.connect_string_user}"
 password = "${var.connect_string_secret}"
 }*/
-}
